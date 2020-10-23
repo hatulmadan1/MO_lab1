fb121cd6e42e5d6ae0380c7060446d3b36c8554f
Hatulmadan <katiq@inbox.ru>
2020-10-23 14:17:14 +0300

    Fixed

--- MO_lab1/ParabolicMethod.cs
+++ MO_lab1/ParabolicMethod.cs
@@ -12,35 +12,22 @@ namespace MO_lab1
             double x1 = data.Start, x3 = data.End, x2 = (x1 + x3) / 2;
             double f1 = data.Function.Invoke(x1), f2 = data.Function.Invoke(x2), f3 = data.Function.Invoke(x3);
             int precision = GetPrecision(data.Epsilon);
+            Random random = new Random();
 
-            Console.WriteLine($"    x1       x3        u        f(u)     l2/l1");
+            Console.WriteLine($"\n{"x1",13} {"x3",9} {"u",9} {"f(u)",9} {"x2",9} {"f(x2)",9} {"l2/l1",8}");
             double length = x3 - x1;
             int i = 1;
             while (Math.Round((x3 - x1), precision) >= data.Epsilon)
             {
                 double u = x2 - (Math.Pow((x2 - x1), 2) * (f2 - f3) - Math.Pow((x2 - x3), 2) * (f2 - f1)) /
                     (2 * ((x2 - x1) * (f2 - f3) - (x2 - x3) * (f2 - f1)));
+                u = Double.IsNaN(u) ? 0.0 : u;
 
                 double fu = data.Function.Invoke(u);
 
-                Console.Write($"{i++,2}{")",-1} {x1:F8} {x2:F8} {x3:F8} {u:F8} {fu:F8} ");
+                Console.Write($"{i++,2}{")",-1} {x1,9:F6} {x3,9:F6} {u,9:F6} {fu,9:F6} {x2,9:F6} {f2,9:F6}");
 
-                if (u > x2)
-                {
-                    if (fu >= f2)
-                    {
-                        x3 = u;
-                        f3 = fu;
-                    }
-                    else
-                    {
-                        x1 = x2;
-                        f1 = f2;
-                        x2 = u;
-                        f2 = fu;
-                    }
-                }
-                else
+                if (u < x2)
                 {
                     if (fu >= f2)
                     {
@@ -51,12 +38,26 @@ namespace MO_lab1
                     {
                         x3 = x2;
                         f3 = f2;
-                        x2 = u;
-                        f2 = fu;
+                    }
+                }
+                else
+                {
+                    if (fu >= f2)
+                    {
+                        x3 = u;
+                        f3 = fu;
+                    }
+                    else
+                    {
+                        x1 = x2;
+                        f1 = f2;
                     }
                 }
 
-                Console.WriteLine($"{(length / (x3 - x1)):F6}");
+                x2 = random.NextDouble() * (x3 - x1) + x1;
+                f2 = data.Function.Invoke(x2);
+
+                Console.WriteLine($"{(length / (x3 - x1)),9:F6}");
                 length = x3 - x1;
             }
 
